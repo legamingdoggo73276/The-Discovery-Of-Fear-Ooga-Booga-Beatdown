b33c7a68608b56b7259903fffaa122ea8de0898d
Dylan L. <dylanl@hotmail.ca>
2025-05-02 14:34:49 -0400

    Combat Commit 2 - Speed System
    Added a speed system to the combat to determine who does damage first.

--- fight.py
+++ fight.py
@@ -1,10 +1,10 @@
 import random
 
 #Function for the battle. Takes 5 parameters (1 string 4 ints) and will take the player stats from Stats.py once that works.
-def battle(enemyType, enemyStr, enemyHP, playerStr, playerHP):
+def battle(enemyType, enemyStr, enemyHP, playerStr, playerHP, playerSpeed, enemySpeed):
 
     #Message displayed once at beginning of battle
-    message = "An", enemyType, "attacks you! \nStrength:", enemyStr, "\nHP:", enemyHP, "\n \nYour strength:", playerStr, "\nYour HP:", playerHP
+    message = f"A {enemyType} attacks you! \nStrength: {enemyStr} \nHP: {enemyHP}\n \nYour strength: {playerStr}\nYour HP: {playerHP}"
 
     #this loop keeps going until the end of the battle
     fighting = True
@@ -16,16 +16,25 @@ def battle(enemyType, enemyStr, enemyHP, playerStr, playerHP):
 
         #if the user chooses to attack
         if action == 0:
-            enemyHP -= playerStr
-            if enemyHP <= 0:
-                fighting = False
-            playerHP -= enemyStr
-            if playerHP <= 0:
-                return "player loss"
-            
-            #new message describing the attack
-            message = "You attacked the", enemyType, "! \n Enemy HP:", enemyHP, "\nPlayer HP:", playerHP
-        
+            #checks to see if player is faster then enemy
+            if playerSpeed >= enemySpeed:
+                enemyHP -= playerStr
+                if enemyHP <= 0:
+                    fighting = False
+                playerHP -= enemyStr
+                if playerHP <= 0:
+                    return "player loss"
+                message = f'You attacked the {enemyType}, dealing {playerStr} damage! \nThe {enemyType} attacks you, dealing {enemyStr} damage. \nEnemy HP: {enemyHP} \nPlayer HP:  {playerHP}'
+            #same code except if the enemy is faster
+            else:
+                playerHP -= enemyStr
+                if playerHP <= 0:
+                    return "player loss"
+                enemyHP -= playerStr
+                if enemyHP <= 0:
+                    fighting = False
+                message = f'The {enemyType} attacks you, dealing {enemyStr} damage. \nYou attacked the {enemyType}, dealing {playerStr} damage! \nEnemy HP: {enemyHP} \nPlayer HP:  {playerHP}'
+                
         #This is a placeholder until we code inventory.
         elif action == 1:
             print("Bag opened")
@@ -43,7 +52,7 @@ def battle(enemyType, enemyStr, enemyHP, playerStr, playerHP):
                 if playerHP <= 0:
                     return "player loss"
                 #new message describing failed escape. Sucess does not need a message as loop is exited.
-                message = "You could not get away. The ghoul attacked you.\nEnemy HP:", enemyHP, "\nPlayer HP:", playerHP
+                message = f"You could not get away. The ghoul attacked you.\nEnemy HP: {enemyHP} \nPlayer HP: {playerHP}"
 
         #This will be removed eventually. It is to find errors.
         else:
@@ -54,4 +63,4 @@ def battle(enemyType, enemyStr, enemyHP, playerStr, playerHP):
     return "enemy vanquished"
 
 
-print(battle("ghoul", 5, 50, 10, 100))
+print(battle("ghoul", 5, 50, 10, 100, 10, 10))
--- stats.py
+++ stats.py
@@ -3,3 +3,4 @@ baseStr = 10
 playerStr = 10
 maxPHP = 100
 playerHP = 100
+playerSpeed = 10

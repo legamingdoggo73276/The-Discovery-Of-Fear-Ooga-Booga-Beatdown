1622aab05971699b66ba9962cd5be95d4fbd4cda
Amberly <amberly.huang@student.tdsb.on.ca>
2025-05-07 15:22:47 -0400

    Update healthbar.py

--- health/healthbar.py
+++ health/healthbar.py
@@ -1,3 +1,14 @@
+import pygame
+
+pygame.init()
+
+#1200 x 800
+screen_width = 1200
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Health Bar')
+
 class HealthBar():
     #for the actual health bar visual
     # # x, y coordinates, width and height (of the health bar rectangle)
@@ -12,11 +23,27 @@ class HealthBar():
     def draw(self, surface):
         #calculate health ratio
         ratio = self.hp / self.max_hp
-        pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
-        pygame.draw.rect(surface, "green", (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(surface, "black", (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(surface, "red", (self.x, self.y, self.w * ratio, self.h))
 
-health_bar = HealthBar(250, 200, 300, 40, 100)
+#health_bar = HealthBar(250, 200, 300, 40, 100)   original
+health_bar = HealthBar(980, 50, 200, 30, 100)
 
-#call upon screen to draw healthbar onto window
+#call upon screen to draw health bar onto window
     #draw health bar
     #health_bar.draw(screen)
+
+run = True
+while run:
+    screen.fill("indigo")
+    
+    health_bar.hp = 50
+    health_bar.draw(screen)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    pygame.display.flip()
+
+pygame.quit()

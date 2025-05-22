a60d4089ab35eb8cbaffb81492f1280fdc6d5b87
Amberly <160063986+amberlyhuang47562@users.noreply.github.com>
2025-05-22 14:19:17 -0400

    Update master_audio.py

--- audio/master_audio.py
+++ audio/master_audio.py
@@ -3,10 +3,29 @@ pygame.init()
 from pygame import mixer
 mixer.init()
 
-def audio(sound):
-    pygame.mixer.Sound.play(sound)
+def music(audio):
+    #-1 repeats sound forever
+    pygame.mixer.Sound.play(audio, -1)
 
-#can keep them as indiviual variables or just a list and call from list !!
-song_collection = ["button.mp3", "campfire.mp3"]
-play_song = pygame.mixer.Sound(song_collection[1])
-audio(play_song)
+def sound(audio):
+    pygame.mixer.Sound.play(audio)
+
+#can keep them as individual variables or just a list and call from list !!
+music_collection = ["campfire.mp3", "drip_cave.mp3"]
+sound_collection = ["button.mp3", "obtain_item.mp3"]
+
+
+#background soundtracks
+campfire = pygame.mixer.Sound(music_collection[0])
+#cave sound is lowkey kinda loud.. fix?
+cave = pygame.mixer.Sound(music_collection[1])
+
+#sound effects
+button = pygame.mixer.Sound(sound_collection[0])
+
+#repeats background noise until canceled (hopefully)
+music(campfire)
+music(cave)
+
+
+sound(button)

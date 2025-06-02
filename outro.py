f40743049239cbfbd3b0a1929f1ef22295bd441c
Amberly <160063986+amberlyhuang47562@users.noreply.github.com>
2025-06-02 15:11:13 -0400

    Update outro.py

--- outro.py
+++ outro.py
@@ -1,5 +1,27 @@
-#pip install opencv-python 
+class cell_9(Map):
+    #import white screen (image)
+    colour = pygame.transform.scale(caveend, (window_width, window_height))
+    stage = "blank"
+    rects = []
+    def __init__(self):
+        Map.bg = self.colour
+        Map.stage = self.stage
+        Map.obstacles = self.rects
+        Map()
+    
+    def blits(self):
+        win.blit(self.colour, (0, 0))
+        
+        if Map.facing == "up":
+            win.blit(back, player)
+        elif Map.facing == "right":
+            win.blit(right, player)
+        elif Map.facing == "left":
+            win.blit(left, player)
+        else:
+            win.blit(front, player)
 
+#pip install opencv-python 
 import cv2
 
 class cell_9(Map):
